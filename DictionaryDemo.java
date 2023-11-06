8bcb2fa1c31541f5ba137f92ca52d3dfa10f136a
MesfinGithinji <meshgithinji@gmail.com>
2023-11-06 23:40:58 +0300

    HashMaps in java

---
+++ DictionaryDemo.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DictionaryDemo {
+
+    public static void main(String[] args) {
+
+        Map<String, String> phoneBook = new HashMap<>();
+
+        phoneBook.put("James Mike", "0722345678");
+        phoneBook.put("Andre Wilson", "01114345678");
+        phoneBook.put("Jasmine Rebecca", "0205345678");
+
+        // Printing the entire dictionary (hash map)
+        System.out.println("This is the entire Dictionary(HashMap)");
+        System.out.println(phoneBook);
+
+        // Retrieving a value based on its key
+        System.out.println("The value mapped to the key Andre Wilson is: " + phoneBook.get("Andre Wilson"));
+     
+        // Printing all the keys
+	System.out.println("\nHere are all the keys:");
+        System.out.println(phoneBook.keySet());
+
+        // Printing all the values
+	System.out.println("\nHere are all the values:");
+        System.out.println(phoneBook.values());
+    }
+}
