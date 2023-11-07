4ec2170f3308c97da6da176c9dfd5c830f38911a
MesfinGithinji <meshgithinji@gmail.com>
2023-11-07 23:37:18 +0300

    Data types in Java

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+
+	public static void main(String[] args){
+		/* Java is a Strongly Typed language
+		 * Type checking is done at compile time 
+		 * declare the type of a value before the variable
+		 *
+		 * Primitive Types
+		 * Reference Types
+		 * */
+		int x=12;
+		double price = 15.94;
+		char grade = 'A';
+		float weight = 24.90;
+		boolean flag = true;
+
+		String name = "Hello";
+
+
+	}
+
+}
