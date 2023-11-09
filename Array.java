67a5421401df22a7bd8f260afe53761b175f5adb
MesfinGithinji <meshgithinji@gmail.com>
2023-11-09 23:37:49 +0300

    Decalre array dynamically

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+	public static void main (String[] args){
+		int[] myArr = {28,48,32,19,34,78,9};
+
+		for (int x=0; x<myArr.length;x++){
+			System.out.println(myArr[x]);
+		}
+	}
+}
