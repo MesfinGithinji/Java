fc4725e04e0ff3cd9adbd71484296b461f0ce850
MesfinGithinji <meshgithinji@gmail.com>
2023-11-15 15:06:52 +0300

    Check if a number is 0 or a negative and replace it with one

---
+++ ReplaceArray.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class ReplaceArray {
+	public static void main (String[] args) throws IOException {
+		BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your ten numbers: ");
+
+		//lets get our space separated values into an array
+		String[] inputValues = bufferedReader.readLine().split(" ");
+		
+		//create a new array that will store the input values
+		int[] arr = new int[10];
+
+		for (int i=0; i < inputValues.length; i++){
+			arr[i] = Integer.parseInt(inputValues[i]);
+			//check if the number is a negative or a positive
+			if (arr[i] < 0 || arr[i] == 0 ){
+				arr[i] = 1 ;
+			}
+		}//end of for loop that loops through the input values
+
+		for (int x=0; x < arr.length; x++){
+			System.out.println("arr["+x+"]="+arr[x]);
+		}//print the value at each index
+		
+	}
+}
