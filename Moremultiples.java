4b9e91e57222825bbf06d56d315aaf9cb1b68f84
MesfinGithinji <meshgithinji@gmail.com>
2023-11-16 23:51:48 +0300

    Comments

--- Moremultiples.java
+++ Moremultiples.java
@@ -4,8 +4,10 @@ import java.io.InputStreamReader;
 
 public class Moremultiples {
 	public static void main (String[] args) throws IOException{
+		//create the buffer
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		
+		//prompt the user
 		System.out.println("Enter the number ");
 		int num = Integer.parseInt(bufferedReader.readLine().trim());
 		
