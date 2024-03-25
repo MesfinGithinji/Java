1a7ee484136276a723e8ef6f0ab74cb1124cd8cb
MesfinGithinji <meshgithinji@gmail.com>
2024-03-25 23:41:42 +0300

    Oreder revision

---
+++ printingOrder.java
@@ -0,0 +1,9 @@
+public class printingOrder {
+    public static void main(String[] args) {
+        int exmpl = 1_000_000;
+        String line = "A";
+        System.out.println('A');
+        System.out.println(line);
+        System.out.println(exmpl);
+    }
+}
