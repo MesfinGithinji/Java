569ddd04bb1e79818f5a15d036e102128d1165d0
MesfinGithinji <meshgithinji@gmail.com>
2024-04-18 09:56:34 +0300

    More on OOP

---
+++ oop/Dogs/Dogs.java
@@ -0,0 +1,19 @@
+public class Dogs {
+
+	private String name;
+	private String breed;
+
+	public Dogs(String name, String breed){
+		this.name = name;
+		this.breed = breed;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getBreed(){
+		return breed;
+	}
+
+}
