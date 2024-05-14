92debb1423bb66aa6102587f8ad5ffbf1f8a49b2
MesfinGithinji <meshgithinji@gmail.com>
2024-05-14 22:54:44 +0300

    OOP in java

--- oop/Dogs/Dogs.java
+++ oop/Dogs/Dogs.java
@@ -1,17 +1,13 @@
 public class Dogs {
-
 	private String name;
 	private String breed;
-
 	public Dogs(String name, String breed){
 		this.name = name;
 		this.breed = breed;
 	}
-
 	public String getName(){
 		return name;
 	}
-
 	public String getBreed(){
 		return breed;
 	}
